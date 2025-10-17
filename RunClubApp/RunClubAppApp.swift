f6658d6e03c326e823682a07457d9076411a1a3e
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-10-17 16:45:41 -0700

    Initial Commit

---
+++ RunClubApp/RunClubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunClubAppApp.swift
+//  RunClubApp
+//
+//  Created by Chiraphat Techasiri on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RunClubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
