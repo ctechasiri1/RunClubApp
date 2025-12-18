e0820c6a18223d048585c8e456afca518af47193
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-12-17 21:40:02 -0800

    Added some source code for HealthKit

---
+++ RunClubApp/Managers/HealthKitManager.swift
@@ -0,0 +1,15 @@
+//
+//  HealthKitManager.swift
+//  RunClubApp
+//
+//  Created by Chiraphat Techasiri on 12/14/25.
+//
+
+import HealthKit
+import Foundation
+
+class HealthKitManager {
+    static let shared = HealthKitManager()
+    static let healthStore = HKHealthStore()
+
+}
